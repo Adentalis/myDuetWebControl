47fbeb26b1208f3459656c34978712aab0102c8b
Adentalis <daniel.n96@t-online.de>
2021-08-06 00:09:01 +0100

    add Icons to Control view

--- src/routes/Control/Control.vue
+++ src/routes/Control/Control.vue
@@ -1,7 +1,7 @@
 <style scoped>
 #homegrid {
   display: grid;
-  grid-template-columns: repeat(4, 35vh);
+  grid-template-columns: repeat(3, 1fr);
   grid-template-rows: minmax(30vh, 30vh);
   grid-gap: 10vh 5vh;
   margin: 5vh;
@@ -10,7 +10,7 @@
 .bigRouter {
   background-color: rgb(126, 123, 123);
   color: rgb(255, 255, 255);
-  height: 34vh;
+  height: 120%;
   text-decoration: none;
   text-transform: uppercase;
   display: inline-block;
@@ -22,10 +22,15 @@
 }
 
 .bigRouter img {
-  max-width: 110px;
+  max-width: 30%;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 20px;
+  margin-top: 5%;
+  padding-top: 3%;
+}
+
+.bigRouter h3 {
+  padding-top: 5%;
 }
 
 .bigRouter:hover {
@@ -40,24 +45,45 @@
   <div id="homegrid">
     <router-link class="bigRouter" to="/Control/Tools">
       <div>
-        <img src="/img/icons/print.png" />
+        <img src="/img/icons/tool.png" />
         <h3 style="text-align: center">Tools</h3>
       </div>
     </router-link>
-    <router-link class="bigRouter" to="/Control/Effector">Effector</router-link>
-    <router-link class="bigRouter" to="/Job/GCodeViewer"
-      >PrintViewer - in print</router-link
-    >
 
-    <router-link class="bigRouter" to="/Files/Extrusion"
-      >Filament - in Tools</router-link
-    >
-    <router-link class="bigRouter" to="/Control/Fans">Fans</router-link>
-    <router-link class="bigRouter" to="/HeightMap">Heightmap</router-link>
+    <router-link class="bigRouter" to="/Control/Effector">
+      <div>
+        <img src="/img/icons/movement.png" />
+        <h3 style="text-align: center">Effector</h3>
+      </div>
+    </router-link>
 
-    <router-link class="bigRouter" to="/Control/Macros">Macros</router-link>
+    <router-link class="bigRouter" to="/Control/Fans">
+      <div>
+        <img src="/img/icons/fan.png" />
+        <h3 style="text-align: center">Fans</h3>
+      </div>
+    </router-link>
 
-    <router-link class="bigRouter" to="/Control/Console">Console</router-link>
+    <router-link class="bigRouter" to="/HeightMap">
+      <div>
+        <img src="/img/icons/map.png" />
+        <h3 style="text-align: center">Heightmap</h3>
+      </div>
+    </router-link>
+
+    <router-link class="bigRouter" to="/Control/Macros">
+      <div>
+        <img src="/img/icons/macro.png" />
+        <h3 style="text-align: center">Macros</h3>
+      </div>
+    </router-link>
+
+    <router-link class="bigRouter" to="/Control/Console">
+      <div>
+        <img src="/img/icons/console.png" />
+        <h3 style="text-align: center">Console</h3>
+      </div>
+    </router-link>
   </div>
 </template>
 
